d8efa5e29d7262554761e6e15ceaeab45e72a3be
Baltzar <baltzar@hotmail.se>
2017-04-20 15:40:57 +0200

    a

---
+++ BreakAwayConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using DataAccess;
+
+namespace BreakAwayConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertDestination();
+        }
+
+        private static void InsertDestination()
+        {
+            var destination = new Destination
+            {
+                Country = "Indonesia",
+                Description = "EcoTourism at its best in exquisiste Bali",
+                Name = "Bali"
+            };
+            using (var context = new BreakAwayContext())
+            {
+                context.Destinations.Add(destination);
+                context.SaveChanges();
+            }
+        }
+    }
+}
